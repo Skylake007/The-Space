6984722597276e661e929769e3399cb495c7ad06
Bình Thanh <thanhbinhdev08@gmail.com>
2023-06-28 10:18:40 +0700

    all basic functions, still waiting for update

---
+++ Assets/_Data/Object/ObjMoveFoward.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjMoveFoward : ObjMovement
+{
+    [Header("Move Foward")]
+    [SerializeField] protected Transform moveTarget;
+    protected override void FixedUpdate()
+    {
+        this.GetMousePosition();
+        base.FixedUpdate();
+    }
+
+	protected override void LoadComponents()
+	{
+		base.LoadComponents();
+        this.LoadTarget();
+	}
+
+    protected virtual void LoadTarget()
+    {
+        if (this.moveTarget != null) return;
+        this.moveTarget = transform.Find("MoveTarget");
+        Debug.LogWarning(transform.name + ": LoadMoveTarget", gameObject);
+    }
+
+	protected virtual void GetMousePosition()
+    {
+        this.targetPosition = this.moveTarget.position; 
+        this.targetPosition.z = 0;
+    }
+
+}
---
+++ Assets/_Data/UI/HPBar/HpSlider.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpSlider : BaseSlider
+{
+	[Header("UI HP Ship")]
+	[SerializeField] protected float maxHP = 50;
+	[SerializeField] protected float currentHP = 50;
+	protected override void OnChange(float newValue)
+	{
+		//Debug.Log("newValue: " + newValue);
+	}
+	protected override void FixedUpdate()
+	{
+		this.HPShowing();
+	}
+
+	protected virtual void HPShowing()
+	{
+		int hpMax = PlayerCtrl.Instance.CurrentShip.DamageReceiver.HPMax;
+		int hp = PlayerCtrl.Instance.CurrentShip.DamageReceiver.HP;
+		float hpPercent = hp / maxHP;
+		this.slider.value = hpPercent;
+	}
+}
